32198452583c5af3b8676c0f2990490979127c2b
Wiktor Ogrodnik <wiktor2800-o@outlook.com>
2022-02-16 14:51:51 +0100

    First thread is working as expected

--- src/main.c
+++ src/main.c
@@ -6,14 +6,27 @@
 #include <pthread.h>
 #include <stdbool.h>
 #include <string.h>
+#include <semaphore.h>
 
 #define ulong unsigned long
+
+#define READER_BUFFER_SIZE 20
 
 typedef struct cpu_raw_data {
     char cpu_name [6];
     ulong data [10];
 } cpu_raw_data;
 
+typedef struct cpu_raw_data_set {
+    size_t size;
+    cpu_raw_data* proc [100];
+} cpu_raw_data_set;
+
+cpu_raw_data_set* reader_buffer[READER_BUFFER_SIZE];
+size_t reader_buffer_size;
+
+sem_t reader_buffer_empty;
+sem_t reader_buffer_full;
 
 void* readstats() {
     
@@ -24,8 +37,9 @@ void* readstats() {
         If queue is full not commit any new rows
     */
     FILE* procstat;
-    
 
+    int debug_counter = 0;
+    
     while (true) {
         char* line = NULL;
         size_t length = 0;
@@ -34,6 +48,9 @@ void* readstats() {
         if ((procstat = fopen("/proc/stat", "r")) == NULL) {
             exit(EXIT_FAILURE);
         }
+
+        cpu_raw_data_set* data_set = (cpu_raw_data_set*)malloc(sizeof(cpu_raw_data_set));
+        data_set->size = 0;
 
         while((read = getline(&line, &length, procstat)) != -1) {
             if (strncmp(line, "cpu", 3) == 0) {
@@ -57,9 +74,8 @@ void* readstats() {
                     
 
                     if (it++) {
-                        cpu_entry->data[it - 2] = atoi(line);
+                        cpu_entry->data[it - 2] = atol(line);
                     } else {
-                        //cpu_entry->cpu_name = (char*)malloc(sizeof(char)*6);
                         strcpy(cpu_entry->cpu_name, line);
                     }
 
@@ -67,19 +83,27 @@ void* readstats() {
                         line = space + 1;
                 }
 
-                printf("Retrieved data: %s ", cpu_entry->cpu_name);
-                for (int i = 0; i < 10; i++) {
-                    printf("%lu ", cpu_entry->data[i]);
-                }
-                printf("\n");
+                // printf("Retrieved data: %s ", cpu_entry->cpu_name);
+                // for (int i = 0; i < 10; i++) {
+                //     printf("%lu ", cpu_entry->data[i]);
+                // }
+                // printf("\n");
 
-                free(cpu_entry);
+                data_set->proc[data_set->size++] = cpu_entry;
 
                 line = lineorigin;
             }
         }
-        free(line);
+
         fclose(procstat);
+        free(line);
+
+        sem_wait(&reader_buffer_empty);
+        reader_buffer[reader_buffer_size++] = data_set;
+        sem_post(&reader_buffer_full);
+
+        printf("DEBUG: %d\n", debug_counter++);
+
     }
 
     return NULL;
@@ -88,6 +112,11 @@ void* readstats() {
 int main() {
 
     int error = 0;
+
+    sem_init(&reader_buffer_empty, 0, READER_BUFFER_SIZE);
+    sem_init(&reader_buffer_full, 0, 0);
+
+    reader_buffer_size = 0;
 
     pthread_t reader;
     error |= pthread_create(&reader, NULL, &readstats, NULL);
@@ -98,6 +127,9 @@ int main() {
     }
 
     pthread_join(reader, NULL);
+
+    sem_destroy(&reader_buffer_empty);
+    sem_destroy(&reader_buffer_full);
 
     return EXIT_SUCCESS;
 }
